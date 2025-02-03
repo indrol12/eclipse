962cbe02e09fcfe1b8e5400c8d3bcfa46b83b672
indrol12 <indrolofficial@gmail.com>
2025-02-03 16:58:49 +0530

    new file feb added!

---
+++ jan31/src/jan31/feb3rd.java
@@ -0,0 +1,14 @@
+package jan31;
+
+public class feb3rd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("numbers from 0-10");
+for(int i=0;i<=10;i++)
+{
+	System.out.println(i);
+}
+	}
+
+}
